9fec91b2bb0dcdca1e83118394390c330ef255aa
james-owen <james@jamesowen.io>
2019-04-15 10:52:08 -0400

    test

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-'use strict';
-
-const express = require('express'),
-    path = require('path');
+const express = require('express');
+const path = require('path');
 
 // Plugin that adds routes for static folder in each site
 module.exports = (router, db, publish, sites, site) => {
-  router.use(express.static(path.join(site.dir, 'static'), {index: false}));
+  router.use(express.static(path.join(site.dir, 'static'), { index: false }));
 };
